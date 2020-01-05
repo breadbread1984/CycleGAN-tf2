511a723d9898fe00d7d5453b7722d1103eae246c
breadbread1984 <breadbread1984@163.com>
2020-01-05 17:12:21 +0800

    update create dataset

--- create_dataset.py
+++ create_dataset.py
@@ -1,10 +1,25 @@
 #!/usr/bin/python3
 
-from os import listdir;
-from os.path import join;
+from os import listdir, mkdir;
+from os.path import join, exists;
 import numpy as np;
 import cv2;
 import tensorflow as tf;
+
+def parse_function_generator(img_shape):
+  def parse_function(serialized_example):
+
+    feature = tf.io.parse_single_example(
+      serialized_example,
+      features = {
+        'data': tf.io.FixedLenFeature((), dtype = tf.string, default_value = '')
+      }
+    );
+    data = tf.io.decode_raw(feature['data'], out_type = tf.uint8);
+    data = tf.reshape(data, img_shape);
+    data = tf.cast(data, dtype = tf.float32);
+    return data, None;
+  return parse_function;
 
 def write_tfrecord(directory, filename):
 
@@ -25,5 +40,6 @@ def write_tfrecord(directory, filename):
 if __name__ == "__main__":
 
   assert tf.executing_eagerly();
-  write_tfrecord('A','A.tfrecord');
-  write_tfrecord('B','B.tfrecord');
+  if False == exists('dataset'): mkdir('dataset');
+  write_tfrecord('A','dataset/A.tfrecord');
+  write_tfrecord('B','dataset/B.tfrecord');
