be2056ca31fa54bb49340bbd37c32c2b0db8e6c4
breadbread1984 <breadbread1984@163.com>
2020-01-05 11:19:42 +0800

    update cyclegan

--- models.py
+++ models.py
@@ -58,6 +58,9 @@ def Discriminator(input_filters, inner_filters, layers = 3):
 
 class CycleGAN(tf.keras.Model):
 
+  IDENTITY_LOSS_WEIGHT = 0.5;
+  CYCLE_LOSS_WEIGHT = 10.0;
+
   def __init__(self, input_filters = 3, output_filters = 3, inner_filters = 64, blocks = 9, layers = 3, ** kwargs):
 
     super(CycleGAN, self).__init__(**kwargs);
@@ -65,16 +68,58 @@ class CycleGAN(tf.keras.Model):
     self.GB = Generator(input_filters = output_filters, output_filters = input_filters, inner_filters = input_filters, blocks = blocks);
     self.DA = Discriminator(input_filters = output_filters, inner_filters = inner_filters, layers = layers);
     self.DB = Discriminator(input_filters = input_filters,  inner_filters = inner_filters, layers = layers);
+    self.bce = tf.keras.losses.BinaryCrossentropy();
+    self.l1 = tf.keras.losses.MeanAbsoluteError();
 
   def call(self, inputs):
 
     real_A = inputs[0];
     real_B = inputs[1];
+    # real_A => GA => fake_B   fake_B => DA => pred_fake_B
+    #                          real_B => DA => pred_real_B
+    # real_B => GB => fake_A   fake_A => DB => pred_fake_A
+    #                          real_A => DB => pred_real_A
     fake_B = self.GA(real_A);
+    pred_fake_B = self.DA(fake_B);
+    pred_real_B = self.DA(real_B);
     rec_A = self.GB(fake_B);
     fake_A = self.GB(real_B);
+    pred_fake_A = self.DB(fake_A);
+    pred_real_A = self.DB(real_A);
     rec_B = self.GA(fake_A);
-    return (fake_B, rec_A, fake_A, rec_B);
+    
+    return (real_A, real_B, fake_B, pred_fake_B, pred_real_B, rec_A, fake_A, pred_fake_A, pred_real_A, rec_B);
+
+  def G_loss(self, inputs):
+    
+    (real_A, real_B, fake_B, pred_fake_B, pred_real_B, rec_A, fake_A, pred_fake_A, pred_real_A, rec_B) = inputs;
+    # generated image should not deviate too much from origin image
+    loss_idt_A = self.l1(fake_B, real_A);
+    loss_idt_B = self.l1(fake_A, real_B);
+    # distance from generated image to natural image
+    loss_GA = self.bce(pred_fake_B, tf.ones_like(pred_B));
+    loss_GB = self.bce(pred_fake_A, tf.ones_like(pred_A));
+    # reconstruction loss
+    loss_cycle_A = self.l1(rec_A, real_A);
+    loss_cycle_B = self.l1(rec_B, real_B);
+    
+    return self.CYCLE_LOSS_WEIGHT * self.IDENTITY_LOSS_WEIGHT * (loss_idt_A + loss_idt_B) + \
+           (loss_GA + loss_GB) + \
+           self.CYCLE_LOSS_WEIGHT * (loss_cycle_A + loss_cycle_B);
+
+  def DA_loss(self, inputs):
+
+    (real_A, real_B, fake_B, pred_fake_B, pred_real_B, rec_A, fake_A, pred_fake_A, pred_real_A, rec_B) = inputs;
+    real_loss = self.bce(pred_real_B, tf.ones_like(pred_real_B));
+    fake_loss = self.bce(pred_fake_B, tf.zeros_like(pred_fake_B));
+    return 0.5 * (real_loss + fake_loss);
+    
+  def DB_loss(self, inputs):
+
+    (real_A, real_B, fake_B, pred_fake_B, pred_real_B, rec_A, fake_A, pred_fake_A, pred_real_A, rec_B) = inputs;
+    real_loss = self.bce(pred_real_A, tf.ones_like(pred_real_A));
+    fake_loss = self.bce(pred_fake_A, tf.zeros_like(pred_fake_A));
+    return 0.5 * (real_loss + fake_loss);
 
 if __name__ == "__main__":
   assert True == tf.executing_eagerly();
