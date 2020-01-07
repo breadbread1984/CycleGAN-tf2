ff38d3660d79965742bb4ded499ac3ed8842981c
breadbread1984 <breadbread1984@163.com>
2020-01-07 15:49:24 +0800

    update dataset downloader

--- download_dataset.py
+++ download_dataset.py
@@ -9,10 +9,10 @@ def main():
   dataset_builder = tfds.builder('cycle_gan/horse2zebra');
   dataset_builder.download_and_prepare();
   # try to load the dataset once
-  train_a = tfds.load(name = 'cycle_gan/horse2zebra', split = tfds.Split.TRAINA, download = False);
-  train_b = tfds.load(name = 'cycle_gan/horse2zebra', split = tfds.Split.TRAINB, download = False);
-  test_a = tfds.load(name = 'cycle_gan/horse2zebra', split = tfds.Split.TESTA, download = False);
-  test_b = tfds.load(name = 'cycle_gan/horse2zebra', split = tfds.Split.TESTB, download = False);
+  train_a = tfds.load(name = 'cycle_gan/horse2zebra', split = tfds.Split.trainA, download = False);
+  train_b = tfds.load(name = 'cycle_gan/horse2zebra', split = tfds.Split.trainB, download = False);
+  test_a = tfds.load(name = 'cycle_gan/horse2zebra', split = tfds.Split.testA, download = False);
+  test_b = tfds.load(name = 'cycle_gan/horse2zebra', split = tfds.Split.testB, download = False);
 
 if __name__ == "__main__":
 
