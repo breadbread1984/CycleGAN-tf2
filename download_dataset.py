c3f0c44955caa87e36db569021a226293b6d7865
breadbread1984 <breadbread1984@163.com>
2020-01-07 14:59:36 +0800

    update dataset downloader

--- download_dataset.py
+++ download_dataset.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 
 import tensorflow as tf;
-import tensorflow_dataset as tfds;
+import tensorflow_datasets as tfds;
 
 def main():
 
   # load dataset
-  dataset_builder = tfds.builder('horse2zebra');
+  dataset_builder = tfds.builder('cycle_gan/horse2zebra');
   dataset_builder.download_and_prepare();
   # try to load the dataset once
-  train_a = tfds.load(name = 'horse2zebra', split = tfds.Split.TRAINA, download = False);
-  train_b = tfds.load(name = 'horse2zebra', split = tfds.Split.TRAINB, download = False);
-  test_a = tfds.load(name = 'horse2zebra', split = tfds.Split.TESTA, download = False);
-  test_b = tfds.load(name = 'horse2zebra', split = tfds.Split.TESTB, download = False);
+  train_a = tfds.load(name = 'cycle_gan/horse2zebra', split = tfds.Split.TRAINA, download = False);
+  train_b = tfds.load(name = 'cycle_gan/horse2zebra', split = tfds.Split.TRAINB, download = False);
+  test_a = tfds.load(name = 'cycle_gan/horse2zebra', split = tfds.Split.TESTA, download = False);
+  test_b = tfds.load(name = 'cycle_gan/horse2zebra', split = tfds.Split.TESTB, download = False);
 
 if __name__ == "__main__":
 
