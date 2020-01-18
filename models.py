2c1c5591b53f3cd3ce16e4b8b6ede457332ccd35
breadbread1984 <breadbread1984@163.com>
2020-01-18 09:39:09 +0800

    update model

--- models.py
+++ models.py
@@ -4,38 +4,40 @@ import numpy as np;
 import tensorflow as tf;
 import tensorflow_addons as tfa;
 
-def Generator(input_filters, output_filters, inner_filters, blocks = 9):
+def Generator(input_filters = 3, output_filters = 3, inner_filters = 64, blocks = 9):
 
   # input
   inputs = tf.keras.Input((None, None, input_filters));
-  results = tf.keras.layers.Lambda(lambda x: tf.pad(x, [[0,0],[3,3],[3,3],[0,0]], 'REFLECT'))(inputs);
-  results = tf.keras.layers.Conv2D(filters = inner_filters, kernel_size = (7,7), padding = 'valid', kernel_initializer = tf.keras.initializers.RandomNormal(stddev = 0.02))(results);
+  results = tf.keras.layers.Conv2D(filters = inner_filters, kernel_size = (7,7), padding = 'same', kernel_initializer = tf.keras.initializers.RandomNormal(stddev = 0.02))(results);
   results = tfa.layers.InstanceNormalization(axis = -1)(results);
   results = tf.keras.layers.ReLU()(results);
   # downsampling
+  # 128-256
   for i in range(2):
-    m = 2**i;
-    results = tf.keras.layers.Conv2D(filters = inner_filters * m * 2, kernel_size = (3,3), strides = (2,2), padding = 'same', kernel_initializer = tf.keras.initializers.RandomNormal(stddev = 0.02))(results);
+    m = 2**(i + 1);
+    results = tf.keras.layers.Conv2D(filters = inner_filters * m, kernel_size = (3,3), strides = (2,2), padding = 'same', kernel_initializer = tf.keras.initializers.RandomNormal(stddev = 0.02))(results);
     results = tfa.layers.InstanceNormalization(axis = -1)(results);
     results = tf.keras.layers.ReLU()(results);
   # resnet blocks
+  # 256
   for i in range(blocks):
     short_circuit = results;
-    results = tf.keras.layers.Lambda(lambda x: tf.pad(x, [[0,0],[1,1],[1,1],[0,0]], 'REFLECT'))(results);
-    results = tf.keras.layers.Conv2D(filters = results.shape[-1], kernel_size = (3,3), padding = 'valid', kernel_initializer = tf.keras.initializers.RandomNormal(stddev = 0.02))(results);
+    results = tf.keras.layers.Conv2D(filters = inner_filters * 4, kernel_size = (3,3), padding = 'same', kernel_initializer = tf.keras.initializers.RandomNormal(stddev = 0.02))(results);
     results = tfa.layers.InstanceNormalization(axis = -1)(results);
     results = tf.keras.layers.ReLU()(results);
-    results = tf.keras.layers.Dropout(rate = 0.5)(results);
-    results = tf.keras.layers.Add()([short_circuit, results]);
+    results = tf.keras.layers.Conv2D(filters = inner_filters * 4, kernel_size = (3,3), padding = 'same', kernel_initializer = tf.keras.initializers.RandomNormal(stddev = 0.02))(results);
+    results = tfa.layers.InstanceNormalization(axis = -1)(results);
+    results = tf.keras.layers.Concatenate()([short_circuit, results]);
   # upsampling
+  # 128-64
   for i in range(2):
-    m = 2**(2 - i);
-    results = tf.keras.layers.Conv2DTranspose(filters = int(inner_filters * m / 2), kernel_size = (3,3), strides = (2,2), padding = 'same', output_padding = 1)(results);
+    m = 2**(1 - i);
+    results = tf.keras.layers.Conv2DTranspose(filters = inner_filters * m, kernel_size = (3,3), strides = (2,2), padding = 'same')(results);
     results = tfa.layers.InstanceNormalization(axis = -1)(results);
     results = tf.keras.layers.ReLU()(results);
   # output
-  results = tf.keras.layers.Lambda(lambda x: tf.pad(x, [[0,0],[3,3],[3,3],[0,0]], 'REFLECT'))(results);
-  results = tf.keras.layers.Conv2D(filters = output_filters, kernel_size = (7,7), padding = 'valid', kernel_initializer = tf.keras.initializers.RandomNormal(stddev = 0.02))(results);
+  results = tf.keras.layers.Conv2D(filters = output_filters, kernel_size = (7,7), padding = 'same', kernel_initializer = tf.keras.initializers.RandomNormal(stddev = 0.02))(results);
+  results = tfa.layers.InstanceNormalization(axis = -1)(results);
   results = tf.keras.layers.Lambda(lambda x: tf.math.tanh(x))(results);
   return tf.keras.Model(inputs = inputs, outputs = results);
 
@@ -44,8 +46,8 @@ def Discriminator(input_filters, inner_filters, layers = 3):
   inputs = tf.keras.Input((None, None, input_filters));
   results = tf.keras.layers.Conv2D(filters = inner_filters, kernel_size = (4,4), strides = (2,2), padding = 'same', kernel_initializer = tf.keras.initializers.RandomNormal(stddev = 0.02))(inputs);
   results = tf.keras.layers.LeakyReLU(0.2)(results);
+  # 128-256-512
   for i in range(layers):
-    # 128-256-512
     m = min(2 ** (i + 1), 8);
     results = tf.keras.layers.Conv2D(filters = inner_filters * m, kernel_size = (4,4), strides = (2,2), padding = 'same', kernel_initializer = tf.keras.initializers.RandomNormal(stddev = 0.02))(results);
     results = tfa.layers.InstanceNormalization(axis = -1)(results);
